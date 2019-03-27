0aa7af75591d34c0d9ddbd0c6faaad1bccba8cf5
Yohei Terashima <yamanogomora@gmail.com>
2019-03-27 05:58:01 +0000

    Edit: 新規ユーザー登録時の入力値をチェックする処理を実装

--- routes/register.js
+++ routes/register.js
@@ -8,11 +8,13 @@ const roles = [
   {num: 2, name: '管理者'},
   {num: 1, name: '実装者'}
 ];
+const message = {};
 
 router.get('/', (req, res, next) => {
   res.render('register', {
     title: '新規ユーザー登録',
-    roles: roles
+    roles: roles,
+    message: message
   });
 });
 
@@ -20,31 +22,46 @@ router.post('/', (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
   const role = req.body.role;
-  User.findOne({
-    where: {
-      username: username
+  // サーバー側でも値のチェック
+  if (!username || !password || !role) {
+    if (!username) {
+      message.username = 'ユーザーネームが未入力です'
     }
-  }).then((user) => {
-    // username の重複をチェック
-    if (user) {
-      const message = 'すでに登録されているユーザーネームです';
-      res.render('register', {
-        title: '新規ユーザー登録',
-        roles: roles,
-        message: message
-      });
-    } else {
-      // DBへの登録処理
-      User.upsert({
-        userId: uuidV1(),
-        username: username,
-        password: password,
-        role: role
-      }).then(() => {
-        res.redirect('/');
-      });
+    if (!password) {
+      message.password = 'パスワードが未入力です'
     }
-  });
+    res.render('register', {
+      title: '新規ユーザー登録',
+      roles: roles,
+      message: message
+    });
+  } else {
+    User.findOne({
+      where: {
+        username: username
+      }
+    }).then((user) => {
+      // username の重複をチェック
+      if (user) {
+        message.username = 'すでに登録されているユーザーネームです';
+        res.render('register', {
+          title: '新規ユーザー登録',
+          roles: roles,
+          message: message
+        });
+      } else {
+        // DBへの登録処理
+        User.upsert({
+          userId: uuidV1(),
+          username: username,
+          password: password,
+          role: role
+        }).then(() => {
+          res.redirect('/');
+        });
+      }
+    });
+  }
 });
 
 module.exports = router;
