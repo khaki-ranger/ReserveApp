9092cd0c155336323bb073d742155409ce9e4fd4
Yohei Terashima <yamanogomora@gmail.com>
2019-03-28 11:27:30 +0000

    Add: DBから取得したデータをスペースに反映する処理を実装

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,39 @@ const express = require('express');
 const router = express.Router();
 const loginUser = require('./login-user');
 const Office = require('../models/office');
+const Space = require('../models/space');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const title = '予約サービス';
   Office.findAll({
-    order: [['"updatedAt"', 'ASC']]
+    order: [['"createdAt"', 'ASC']]
   }).then((offices) => {
-    loginUser(req.user, (result) => {
-      res.render('index', {
-        title: title,
-        loginUser: result,
-        offices: offices
+    Space.findAll({
+      include: [
+        {
+          model: Office,
+          attributes: ['officeId']
+        }
+      ],
+      order: [['"createdAt"', 'ASC']]
+    }).then((spaces) => {
+      offices.forEach((office) => {
+        const officeId = office.officeId;
+        const spacesArray = [];
+        spaces.forEach((space) => {
+          if(officeId === space.officeId) {
+            spacesArray.push(space);
+          }
+        });
+        office['spaces'] = spacesArray;
+      });
+      loginUser(req.user, (result) => {
+        res.render('index', {
+          title: title,
+          loginUser: result,
+          offices: offices
+        });
       });
     });
   });
