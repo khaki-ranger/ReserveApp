7e38c2d87893ed1f2be85ac69ce7938a0dc321c4
Yohei Terashima <yamanogomora@gmail.com>
2019-03-28 13:28:03 +0000

    Add: スケジュールを反映させるための準備

--- routes/index.js
+++ routes/index.js
@@ -5,46 +5,26 @@ const loginUser = require('./login-user');
 const Office = require('../models/office');
 const Space = require('../models/space');
 
-const period = [
+// 仮データ
+// 後にDBから取得する
+const schedules = [
   {
-    periodId: 1,
-    periodname: '9:00 - 10:00',
-    availability: true
+    id: 1,
+    userId: '48cb2540-5041-11e9-a06c-1d597b669eb2',
+    spaceId: '182de6a0-5147-11e9-b2a3-87358e6d1f52',
+    date: '2019-03-28 10:49:00.173+00',
+    periodNum: 6,
+    guestname: '鈴木イチロー',
+    mailaddress: 'suzuki@mail.com'
   },
   {
-    periodId: 2,
-    periodname: '10:00 - 11:00',
-    availability: true
-  },
-  {
-    periodId: 3,
-    periodname: '11:00 - 12:00',
-    availability: false
-  },
-  {
-    periodId: 4,
-    periodname: '12:00 - 13:00',
-    availability: true
-  },
-  {
-    periodId: 5,
-    periodname: '13:00 - 14:00',
-    availability: false
-  },
-  {
-    periodId: 6,
-    periodname: '14:00 - 15:00',
-    availability: false
-  },
-  {
-    periodId: 7,
-    periodname: '15:00 - 16:00',
-    availability: true
-  },
-  {
-    periodId: 8,
-    periodname: '16:00 - 17:30',
-    availability: true
+    id: 2,
+    userId: '48cb2540-5041-11e9-a06c-1d597b669eb2',
+    spaceId: 'a0ded300-514d-11e9-87ec-7dd538d6d6ff',
+    date: '2019-03-28 10:49:00.173+00',
+    periodNum: 2,
+    guestname: '中村紀洋',
+    mailaddress: 'nakamura@mail.com'
   }
 ];
 
@@ -64,11 +44,61 @@ router.get('/', function(req, res, next) {
       order: [['"createdAt"', 'ASC']]
     }).then((spaces) => {
       offices.forEach((office) => {
-        const officeId = office.officeId;
         const spacesArray = [];
         spaces.forEach((space) => {
-          if(officeId === space.officeId) {
-            space['periods'] = period;
+          if(office.officeId === space.officeId) {
+
+            // 取り敢えず
+            // 他に良い方法が無いか探る
+            const periodObj = {
+              1: {
+                num: 1,
+                periodname: '9:00 - 10:00',
+                availability: true
+              },
+              2: {
+                num: 2,
+                periodname: '10:00 - 11:00',
+                availability: true
+              },
+              3: {
+                num: 3,
+                periodname: '11:00 - 12:00',
+                availability: true
+              },
+              4: {
+                num: 4,
+                periodname: '12:00 - 13:00',
+                availability: true
+              },
+              5: {
+                num: 5,
+                periodname: '13:00 - 14:00',
+                availability: true
+              },
+              6: {
+                num: 6,
+                periodname: '14:00 - 15:00',
+                availability: true
+              },
+              7: {
+                num: 7,
+                periodname: '15:00 - 16:00',
+                availability: true
+              },
+              8: {
+                num: 8,
+                periodname: '16:00 - 17:30',
+                availability: true
+              }
+            };
+
+            schedules.forEach((schedule) => {
+              if(space.spaceId === schedule.spaceId) {
+                periodObj[schedule.periodNum].availability = false;
+              }
+            });
+            space['periods'] = periodObj;
             spacesArray.push(space);
           }
         });
