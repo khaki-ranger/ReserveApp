c423a0bff41e19ac6a4d29c07fef358ae336fbb6
Yohei Terashima <yamanogomora@gmail.com>
2019-03-31 10:07:07 +0000

    Add: 予約フォームに情報を送信して、POSTリクエストを送る処理を実装

--- routes/index.js
+++ routes/index.js
@@ -2,57 +2,9 @@
 const express = require('express');
 const router = express.Router();
 const loginUser = require('./login-user');
+const Periods = require('./periods');
 const Office = require('../models/office');
 const Space = require('../models/space');
-
-// ピリオド（コマ）を表現するクラスを定義
-class Periods {
-  constructor() {
-    const periods = {
-      1: {
-        num: 1,
-        periodname: '9:00 - 10:00',
-        availability: true
-      },
-      2: {
-        num: 2,
-        periodname: '10:00 - 11:00',
-        availability: true
-      },
-      3: {
-        num: 3,
-        periodname: '11:00 - 12:00',
-        availability: true
-      },
-      4: {
-        num: 4,
-        periodname: '12:00 - 13:00',
-        availability: true
-      },
-      5: {
-        num: 5,
-        periodname: '13:00 - 14:00',
-        availability: true
-      },
-      6: {
-        num: 6,
-        periodname: '14:00 - 15:00',
-        availability: true
-      },
-      7: {
-        num: 7,
-        periodname: '15:00 - 16:00',
-        availability: true
-      },
-      8: {
-        num: 8,
-        periodname: '16:00 - 17:30',
-        availability: true
-      }
-    };
-    return periods;
-  }
-}
 
 // 仮データ
 // 後にDBから取得する
--- routes/reserve.js
+++ routes/reserve.js
@@ -3,17 +3,57 @@ const express = require('express');
 const router = express.Router();
 const loginUser = require('./login-user');
 const authenticationEnsurer = require('./authentication-ensurer');
+const Office = require('../models/office');
+const Space = require('../models/space');
+const Periods = require('./periods');
 
-router.get('/', authenticationEnsurer, (req, res, next) => {
+router.get('/space/:spaceid/period/:periodnum/year/:year/month/:month/day/:day', authenticationEnsurer, (req, res, next) => {
   const title = '予約 | SERVICE NAME';
   const message = {};
-  loginUser(req.user, (result) => {
-    res.render('reserve', {
-      title: title,
-      loginUser: result,
-      message: message
-    });
+  const periods = new Periods();
+  const date = new Date(req.params.year, req.params.month, req.params.day);
+  const dayofweeknum = date.getDay();
+  const dayofweekstring = ['日', '月', '火', '水', '木', '金', '土'][dayofweeknum];
+  Space.findOne({
+    include: [
+      {
+        model: Office,
+        attributes: ['officeId', 'officename']
+      }
+    ],
+    where: {
+      spaceId: req.params.spaceid
+    }
+  }).then((s) => {
+    if(s) {
+      const params = {
+        officename: s.office.officename, 
+        spaceId: req.params.spaceid,
+        spacename: s.spacename,
+        periodnum: req.params.periodnum,
+        periodname: periods[req.params.periodnum].periodname,
+        year: req.params.year,
+        month: req.params.month,
+        day: req.params.day,
+        dayofweekstring: dayofweekstring
+      };
+      loginUser(req.user, (result) => {
+        res.render('reserve', {
+          title: title,
+          loginUser: result,
+          message: message,
+          params: params
+        });
+      });
+    } else {
+      console.log('スペースが見つかりませんでした');
+    }
   });
+});
+
+router.post('/', authenticationEnsurer, (req, res, next) => {
+  console.log(req.body);
+  res.json(req.body);
 });
 
 module.exports = router;
