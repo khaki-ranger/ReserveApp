b5e1a71f1660442d136079b621c5aa80bc5b3c28
Yohei Terashima <yamanogomora@gmail.com>
2019-04-04 08:05:25 +0000

    Add: 確認メール送信機能 テストメールを送信する処理を実装

--- routes/reserve.js
+++ routes/reserve.js
@@ -1,12 +1,57 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
+require('dotenv').config();
+const MAIL_AUTH_USER = process.env.MAIL_AUTH_USER;
+const MAIL_AUTH_PASSWORD = process.env.MAIL_AUTH_PASSWORD;
+const nodemailer = require('nodemailer');
 const loginUser = require('./login-user');
 const authenticationEnsurer = require('./authentication-ensurer');
 const Periods = require('./periods');
 const Office = require('../models/office');
 const Space = require('../models/space');
 const Reservation = require('../models/reservation');
+
+const sendGmail = ((params) => {
+
+const messageBody = `${params.guestname} 様
+予約確認メールのテストです。
+
+【ご予約施設】
+${params.officename}
+${params.spacename}
+
+【ご予約日時】
+${params.year}年 ${params.month}月 ${params.day}日 (${params.dayofweekstring}) ${params.periodname}
+
+`;
+
+  const message = {
+    from: MAIL_AUTH_USER,
+    to: params.to,
+    subject: '［予約サービス］ご予約完了のお知らせ',
+    text: messageBody
+  };
+  const smtpConfig = {
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: MAIL_AUTH_USER,
+      pass: MAIL_AUTH_PASSWORD
+    }
+  };
+  const transporter = nodemailer.createTransport(smtpConfig);
+  transporter.sendMail(message, (error, response) => {
+    if (error) {
+      console.log(error);
+      // 後でエラー処理をする
+      res.json(error);
+    } else {
+      console.log(response);
+    }
+  });
+});
 
 const getParams = ((params, callback) => {
   const periods = new Periods();
@@ -88,7 +133,27 @@ router.post('/', authenticationEnsurer, (req, res, next) => {
       createdBy: req.user.userId,
       canceled: false
     }).then((r) => {
+      console.log('create reservation');
+      const params = {
+        spaceId: r.spaceId,
+        periodnum: r.periodnum,
+        year: req.body.year,
+        month: req.body.month - 1,
+        day: req.body.day
+      };
+      getParams(params, (error, response) => {
+        if (error) {
+          console.log(err);
+        } else {
+          response.guestname = r.guestname;
+          response.to = r.mailaddress;
+          response.createdAt = r.createdAt;
+          sendGmail(response); 
+        }
+      });
       res.redirect('/');
+    }).catch((error) => {
+      console.log(error);
     });
   }
 });
