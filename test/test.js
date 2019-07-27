739323f5b37c350d1d203b188ff43e54041db764
Yohei Terashima <yamanogomora@gmail.com>
2019-07-27 06:32:20 +0000

    Edit: #53 認証のテストを実装

--- test/test.js
+++ test/test.js
@@ -1,15 +1,36 @@
 'use strict';
 const request = require('supertest');
 const app = require('../app');
+const passportStub = require('passport-stub');
 
 describe('/login', () => {
+  before(() => {
+    passportStub.install(app);
+    passportStub.login({ userId: '86ef67f0-b036-11e9-926d-1338c82e144a', username: 'testUser' });
+  });
 
-  it('ログインのためのフォームが含まれる', (done) => {
+  after(() => {
+    passportStub.logout();
+    passportStub.uninstall(app);
+  });
+
+  it('ログイン時はユーザー名が表示される', (done) => {
     request(app)
       .get('/')
       .expect('Content-Type', 'text/html; charset=utf-8')
-      .expect(/<form action="\/login" method="post".*>/)
+      .expect(/testUser/)
       .expect(200, done);
+  });
+
+});
+
+describe('/logout', () => {
+
+  it('ログアウトした際には、/にリダイレクトされる', (done) => {
+    request(app)
+      .get('/logout')
+      .expect('Location', '/')
+      .expect(302, done);
   });
 
 });
